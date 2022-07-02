1c3534ea4a4aee3367b4878c344770ac2aa747f7
vineethreddyv-acc <108539023+vineethreddyv-acc@users.noreply.github.com>
2022-07-02 12:43:51 +0530

    Update dealerportal_vms.py

--- dealerportal_vms.py
+++ dealerportal_vms.py
@@ -2,6 +2,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
@@ -9,7 +11,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 import time
 
-driver = webdriver.Chrome('C:\SeleniumWebDrivers\ChromeDriver')
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 
 driver.get("https://stg-portal.cnh.com/DPLogin/")
